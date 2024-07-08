f88ef65f60260453c40a4db001ccced0a0db153e
Allen A <allena@Allens-MacBook-Pro.local>
2024-07-08 00:49:54 -0400

    undid last commit accidentally

--- day12_DOM/script.js
+++ day12_DOM/script.js
@@ -11,6 +11,9 @@ let s3 = document.querySelector(".s3");
 s3.style.color = "magenta";
 s3.style.fontSize = "30px";
 
+let title = document.querySelector(h1);
+title.setAttribute("class", "styletitle");
+
 //select s4
 
 let s4 = document.querySelector(".s4");
@@ -24,3 +27,32 @@ s4.innerHTML = "OFF by the <b><em>midnight</em></b>";
 //className
 let spans = document.querySelector("#special-span");
 spans.className = "changefontfamily";
+
+//remove a class name from an element
+//remove the class 'info' from <div class="info">
+let divmoreinfo = document.querySelector(".moreinfo");
+divmoreinfo.classList.remove("info");
+
+// append a text after <div id="special">
+let divspecial = document.querySelector("#special");
+divspecial.append("NEW INFORMATION");
+
+// appendChild to add a new <li> to <ul class="todo">
+// step 1) create <li> element
+let newitem = document.createElement("li");
+
+// step 2) give content to the new <li>
+let newlist = document.createTextNode("Post students' grade");
+newitem.appendChild(newlist);
+
+// step 3) append as child the newitem into the document
+// append to <ul class="todo">
+let todolist = document.querySelector(".todo");
+todolist.appendChild(newitem);
+
+// remove an element using removeChild method
+todolist.removeChild(todolist.children[1]);
+
+// remove an element using remove method
+// select the element that you want to remove and use the method remove
+todolist.children[0].remove();
---
+++ day14_storage/cart.js
@@ -0,0 +1,41 @@
+const priceitem_display = document.querySelector(".priceitem_display");
+const quantity_display = document.querySelector(".quantity_display");
+const totalprice_display = document.querySelector(".totalprice_display");
+const itemimg = document.querySelector(".itemimg");
+const itemdescription = document.querySelector(".itemdescription");
+
+// get values for quantity and price per unit
+const priceitem = sessionStorage.getItem("sale_price");
+const quantityitem = sessionStorage.getItem("quantityitem1");
+const itemimage = sessionStorage.getItem("item_image");
+const description_item = sessionStorage.getItem("description_item1");
+
+// calculate for the total price
+const totalprice = (priceitem * quantityitem).toFixed(2);
+
+// append image and itme description
+let image = document.createElement("img");
+image.src = sessionStorage.getItem("item_image");
+image.style.width = "30px";
+
+itemimg.append(image);
+
+// append product description
+let product = document.createElement("p");
+product.innerHTML = description_item;
+itemdescription.append(product);
+
+// append price
+let productprice = document.createElement("p");
+productprice.innerHTML = priceitem;
+priceitem_display.append(productprice);
+
+// append quantity
+let totalquantity = document.createElement("p");
+totalquantity.innerHTML = quantityitem;
+quantity_display.append(totalquantity);
+
+// append total price
+let alltotalprice = document.createDocumentFragment("p");
+t.innerHTML = `$ ${totalprice}`;
+totalprice_display.append(alltotalprice);
